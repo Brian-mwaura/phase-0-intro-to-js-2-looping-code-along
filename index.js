07e604c860a7c01c689fe22ac8beb7f8526b863b
Brian <briomwaura@gmail.com>
2023-10-22 22:53:16 +0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    var thankYouCards = [];
+    for (var i = 0; i < names.length; i++) {
+      var message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouCards.push(message);
+    }
+    return thankYouCards;
+  }
